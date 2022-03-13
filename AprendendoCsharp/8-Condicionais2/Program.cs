9b6cabec92c3a1d8ff67b56a6c980a15a1d3c243
HarryCSantos <harry.santos@aluno.faculdadeimpacta.com.br>
2022-03-12 21:43:04 -0300

    alt

--- AprendendoCsharp/8-Condicionais2/Program.cs
+++ AprendendoCsharp/8-Condicionais2/Program.cs
@@ -7,6 +7,9 @@ namespace _8_Condicionais2
         static void Main(string[] args)
         {
             Console.WriteLine("Executando projeto 8 - Condicionais 2");
+
+            int idadeJoao = 16;
+
         }
     }
 }

38488dd579753c556d6597747cc30ea834130703
HarryCSantos <harry.santos@aluno.faculdadeimpacta.com.br>
2022-03-12 21:10:05 -0300

    alt

--- AprendendoCsharp/4-ConversoesEOustrosTiposNumericos/Program.cs
+++ AprendendoCsharp/4-ConversoesEOustrosTiposNumericos/Program.cs
@@ -1,12 +1,4 @@
 ﻿using System;
 
 namespace _4_ConversoesEOustrosTiposNumericos
-{
-    class Program
-    {
-        static void Main(string[] args)
-        {
-            Console.WriteLine("Hello World!");
-        }
-    }
-}
+

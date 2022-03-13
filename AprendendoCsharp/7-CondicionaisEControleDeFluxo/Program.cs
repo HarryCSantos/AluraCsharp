c0043a4577ca6c6e26b33bfd7f5ed101d93d76c3
HarryCSantos <harry.santos@aluno.faculdadeimpacta.com.br>
2022-03-12 21:28:26 -0300

    alt

--- AprendendoCsharp/7-CondicionaisEControleDeFluxo/Program.cs
+++ AprendendoCsharp/7-CondicionaisEControleDeFluxo/Program.cs
@@ -1,12 +1,4 @@
 ﻿using System;
 
 namespace _7_CondicionaisEControleDeFluxo
-{
-    class Program
-    {
-        static void Main(string[] args)
-        {
-            Console.WriteLine("Hello World!");
-        }
-    }
-}
+

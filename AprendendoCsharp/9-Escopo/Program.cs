8e827bf17ba29c4e9588d00113453b702a13115f
HarryCSantos <harry.santos@aluno.faculdadeimpacta.com.br>
2022-03-14 17:41:47 -0300

    lat

---
+++ AprendendoCsharp/9-Escopo/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace _9_Escopo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Executando projeto 9 - Escopo");
+        }
+    }
+}

a37eeede24afacf85a685e3c60cb7cdfc00e6116
HarryCSantos <harry.santos@aluno.faculdadeimpacta.com.br>
2022-03-14 19:56:00 -0300

    alt

--- AprendendoCsharp/9-PraticaEscopo/Program.cs
+++ AprendendoCsharp/9-PraticaEscopo/Program.cs
@@ -9,9 +9,10 @@ namespace _9_Escopo
             Console.WriteLine("Prática Escopo - Testando Condicional2");
 
             int idade= 18;
-            int quantidadePessoas = 3;
+            int quantidadePessoas = 1;
             bool acompanhado = quantidadePessoas >=2;
-            
+            //Dessa forma, nossa variável booleana terá valor true quando quantidadePessoas for maior ou igual a 2, e terá valor false quando ocorrer o contrário.
+
             //if (idade >=18 || quantidadePessoas >=2)
             if (idade >=18 && acompanhado)
             {

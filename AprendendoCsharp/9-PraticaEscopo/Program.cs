2ea394ca9a70724f7ac20413ff5bbe101a24db13
HarryCSantos <harry.santos@aluno.faculdadeimpacta.com.br>
2022-03-14 19:20:58 -0300

    alt

--- AprendendoCsharp/9-PraticaEscopo/Program.cs
+++ AprendendoCsharp/9-PraticaEscopo/Program.cs
@@ -1,12 +1,40 @@
 ﻿using System;
 
-namespace _9_PraticaEscopo
+namespace _9_Escopo
 {
     class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Console.WriteLine("Executando projeto 9 - Escopo");
+
+            int idadeJoao = 18;
+            bool acompanhado = false;
+            string mensagemAdicional;
+
+            if (acompanhado == true)
+            {
+                mensagemAdicional = "João está acompanhado!";
+
+            }
+            else
+            {
+                mensagemAdicional = "João não está acompanhado";
+            }
+
+            if (idadeJoao >= 18 && acompanhado == true)
+            {
+                Console.WriteLine("Pode entrar.");
+                Console.WriteLine(mensagemAdicional);
+            }
+            else
+            {
+                Console.WriteLine("Não pode entrar");
+                Console.WriteLine(mensagemAdicional);
+            }
+
+            Console.ReadLine();
+
         }
     }
 }

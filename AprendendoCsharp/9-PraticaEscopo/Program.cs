cb55afeb3ff9e9d25911333f1969f8b7c49019f8
HarryCSantos <harry.santos@aluno.faculdadeimpacta.com.br>
2022-03-14 19:38:23 -0300

    alt

--- AprendendoCsharp/9-PraticaEscopo/Program.cs
+++ AprendendoCsharp/9-PraticaEscopo/Program.cs
@@ -11,7 +11,8 @@ namespace _9_Escopo
             int idade= 16;
             int quantidadePessoas = 3;
             
-            if (idade >=18 || quantidadePessoas >=2)
+          //if (idade >=18 || quantidadePessoas >=2)
+            if (idade >=18 && quantidadePessoas >=2)
             {
                 Console.WriteLine("Seja bem vindo");
             }

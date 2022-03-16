5885dce1c5f8f2d55312f4b3df317dc13c5ae7b4
HarryCSantos <harry.santos@aluno.faculdadeimpacta.com.br>
2022-03-15 21:09:21 -0300

    alt

---
+++ AprendendoCsharp/9.02-TesteIRPF/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace _9._02_TesteIRPF
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            double salario = 3300.0;
+
+            if(salario < 2600.0)
+
+            Console.WriteLine("A sua aliquota é de 15 %");
+        }
+    }
+}

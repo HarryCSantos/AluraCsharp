c0d28773ffda7b35251b53221910ec318b3db643
HarryCSantos <harry.santos@aluno.faculdadeimpacta.com.br>
2022-03-15 21:38:22 -0300

    alt

---
+++ AprendendoCsharp/9.03-Funcionariopromovido/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace _9._03_Funcionariopromovido
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            bool foiPromovido = true;
+
+            Console.WriteLine("Hello World!");
+        }
+    }
+}

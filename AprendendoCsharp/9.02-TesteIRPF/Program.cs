970695bbe2398fb923d5d4292c3d2691d2c34438
HarryCSantos <harry.santos@aluno.faculdadeimpacta.com.br>
2022-03-15 21:31:37 -0300

    alt

--- AprendendoCsharp/9.02-TesteIRPF/Program.cs
+++ AprendendoCsharp/9.02-TesteIRPF/Program.cs
@@ -10,12 +10,16 @@ namespace _9._02_TesteIRPF
             double salario = 3300.0;
 
             if(salario < 2600.0)
+            { 
                 Console.WriteLine("A sua aliquota é de 15 %");
                 Console.WriteLine("Você pode deduzir até R$ 350");
+            }
 
             if (salario < 3750.0)
+            {
                 Console.WriteLine("A sua aliquota é  de 22,5%");
                 Console.WriteLine("Você pode deduzir até R$ 636");
+            }
         }
     }
 }

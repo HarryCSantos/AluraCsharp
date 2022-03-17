1d950eaaa6d6d4306f5d2ce6937a035ec2567f5f
HarryCSantos <harry.santos@aluno.faculdadeimpacta.com.br>
2022-03-16 21:48:11 -0300

    alt

--- AprendendoCsharp/9.05-AliquotaComIfs/Program.cs
+++ AprendendoCsharp/9.05-AliquotaComIfs/Program.cs
@@ -13,6 +13,11 @@ namespace _9._05_AliquotaComIfs
             {
                 Console.WriteLine("Seu imposto de Renda é de 7.5% e pode deduzir na declaração o valor de R$ 142");
             }
+            if
+            {
+                (salario>=2880 )
+            }
+
 
             Console.WriteLine("Testando Alíquota com Ifs!");
 

f46982befab5cd2e94d72379b8bd3ba6f362a22a
HarryCSantos <harry.santos@aluno.faculdadeimpacta.com.br>
2022-03-17 20:50:58 -0300

    alt

--- AprendendoCsharp/9.05-AliquotaComIfs/Program.cs
+++ AprendendoCsharp/9.05-AliquotaComIfs/Program.cs
@@ -7,7 +7,7 @@ namespace _9._05_AliquotaComIfs
         static void Main(string[] args)
         {
 
-            double salario = 3751.0;
+            double salario = 3751.1;
 
             Console.WriteLine("Testando Alíquota com Ifs!");
 

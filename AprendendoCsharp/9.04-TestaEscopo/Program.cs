ba729857737d83b418b4ae59a2450f05ddd71aae
HarryCSantos <harry.santos@aluno.faculdadeimpacta.com.br>
2022-03-16 21:07:16 -0300

    alt

---
+++ AprendendoCsharp/9.04-TestaEscopo/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace _9._04_TestaEscopo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}

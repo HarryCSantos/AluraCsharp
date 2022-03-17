a96ccd2afcea95bba39bcca0d465c268fb8f40ce
HarryCSantos <harry.santos@aluno.faculdadeimpacta.com.br>
2022-03-16 21:29:54 -0300

    alt

---
+++ AprendendoCsharp/9.05-AliquotaComIfs/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace _9._05_AliquotaComIfs
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Alíquota com Ifs!");
+        }
+    }
+}

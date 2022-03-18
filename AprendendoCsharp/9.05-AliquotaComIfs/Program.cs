59101ae7404c76a7ad86865520d048ba7811e927
HarryCSantos <harry.santos@aluno.faculdadeimpacta.com.br>
2022-03-18 20:28:04 -0300

    alt

--- AprendendoCsharp/9.05-AliquotaComIfs/Program.cs
+++ AprendendoCsharp/9.05-AliquotaComIfs/Program.cs
@@ -7,7 +7,8 @@ namespace _9._05_AliquotaComIfs
         static void Main(string[] args)
         {
             // De 1900.0 até 2800.0 o IR é de 7.5% e pode deduzir R$ 142
-            // De 2800
+            // De 2800.01 até 3751.0 o IR é de 15% e pode deduzir R$ 350
+
 
            double salario = 3300.0;
 

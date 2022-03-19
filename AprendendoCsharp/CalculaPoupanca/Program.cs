71c390241b9fed9bebd4f42a1deb903e6e094258
HarryCSantos <harry.santos@aluno.faculdadeimpacta.com.br>
2022-03-19 18:55:28 -0300

    alt

--- AprendendoCsharp/CalculaPoupanca/Program.cs
+++ AprendendoCsharp/CalculaPoupanca/Program.cs
@@ -7,6 +7,13 @@ namespace CalculaPoupanca
         static void Main(string[] args)
         {
             Console.WriteLine("Executando projeto 10 - Calcula Poupança");
+
+            double valorInvestido = 1000;
+
+            //0.36 = 0.0036
+            valorInvestido = valorInvestido + valorInvestido * 0.0036;
+
+            Console.ReadLine();
         }
     }
 }

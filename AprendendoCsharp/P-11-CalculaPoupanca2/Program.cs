ce941e996fb8fdd8a2da52434c95bc23cfe07c9a
HarryCSantos <harry.santos@aluno.faculdadeimpacta.com.br>
2022-03-20 06:22:54 -0300

    alt

--- AprendendoCsharp/P-11-CalculaPoupanca2/Program.cs
+++ AprendendoCsharp/P-11-CalculaPoupanca2/Program.cs
@@ -13,6 +13,9 @@ namespace P_11_CalculaPoupanca2
             for (int contadorMes = 1; contadorMes <= 12; contadorMes++)
             {
                 //valorInvestido = valorInvestido + valorInvestido * 1.0036;
+                
+                //Ao executarmos o código, conseguiremos manter o mesmo comportamento. E agora que só lidamos com valorInvestido e estamos guardando na variável o resultado de uma conta com apenas um operador, podemos substitui-lo por *=. Com isso, o valor da variável será multiplicado por 1.0036 e será guardado novamente em valorInvestido.
+
                 valorInvestido *= 1.0036;
                 Console.WriteLine("Após " + contadorMes + " meses, você terá R$ " + valorInvestido);
 

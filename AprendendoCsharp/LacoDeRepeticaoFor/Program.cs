53a2302633c786f263de49c740c56622b1f52937
HarryCSantos <harry.santos@aluno.faculdadeimpacta.com.br>
2022-03-20 12:41:52 -0300

    alt

--- AprendendoCsharp/LacoDeRepeticaoFor/Program.cs
+++ AprendendoCsharp/LacoDeRepeticaoFor/Program.cs
@@ -2,13 +2,14 @@
 
 class Programa
 {
-    static void Main(String [] arg)
+    static void Main(String [] args)
     {
         for (int i = 1; i < 100; i++)
         {
             if (i % 3 == 0)
             {
                 Console.WriteLine(i);
+
             }
         }
     }

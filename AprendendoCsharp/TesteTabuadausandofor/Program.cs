50bbc500e06e4f8c0dd911cfb9341df580b453a1
HarryCSantos <harry.santos@aluno.faculdadeimpacta.com.br>
2022-03-20 10:34:51 -0300

    alt

--- AprendendoCsharp/TesteTabuadausandofor/Program.cs
+++ AprendendoCsharp/TesteTabuadausandofor/Program.cs
@@ -12,7 +12,7 @@ namespace TesteTabuadausandofor
             {
                 for (int contador = 0; contador <= 10; contador++)
                 {
-
+                    Console.WriteLine(multiplicador + " * " + contador + " = " + multiplicador * contador);
                 }
             }
 

7551d209ab6837b616926f2e300d420b4ec77084
HarryCSantos <harry.santos@aluno.faculdadeimpacta.com.br>
2022-03-19 21:27:41 -0300

    alt

--- AprendendoCsharp/CalculaPoupanca/Program.cs
+++ AprendendoCsharp/CalculaPoupanca/Program.cs
@@ -14,7 +14,7 @@ namespace CalculaPoupanca
             while (contadorMes <= 12)
             {
                 valorInvestido = valorInvestido + valorInvestido * 0.0036;
-                Console.WriteLine("Após " + contadorMes + " meses, você terá    R$ " + valorInvestido);
+                Console.WriteLine("Após " + contadorMes + " meses, você terá R$ " + valorInvestido);
 
                 //contadorMes = contadorMes + 1;
                 //contadorMes += 1; --> o contadorMes++; incrementa mais um e guarda na variável

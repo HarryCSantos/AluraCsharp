da8ad8de25e0b54d1fc6c5697a052621f71993c8
HarryCSantos <harry.santos@aluno.faculdadeimpacta.com.br>
2022-03-20 08:30:15 -0300

    alt

--- AprendendoCsharp/P12-CalculaInvestimentoALongoPrazo/Program.cs
+++ AprendendoCsharp/P12-CalculaInvestimentoALongoPrazo/Program.cs
@@ -6,7 +6,10 @@ namespace P12_CalculaInvestimentoALongoPrazo
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Console.WriteLine("Executando o projeto 12");
+
+            double fatorRendimento = 1.0036;
+
         }
     }
 }

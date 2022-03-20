d4d45bac505a8a61380da495fa058db6e9b441fe
HarryCSantos <harry.santos@aluno.faculdadeimpacta.com.br>
2022-03-20 10:53:34 -0300

    alt

--- AprendendoCsharp/LacoDeRepeticaoFor/Program.cs
+++ AprendendoCsharp/LacoDeRepeticaoFor/Program.cs
@@ -7,6 +7,16 @@ namespace LacoDeRepeticaoFor
         static void Main(string[] args)
         {
             Console.WriteLine("Executando Programa: Criando uma Matrix Triangular");
+
+            for (int linha = 0; linha < 10; linha++)
+            {
+                for(int coluna = 0; coluna < 10; coluna++)
+                {
+                    if (coluna > linha)
+                }
+            }
+
+
         }
     }
 }

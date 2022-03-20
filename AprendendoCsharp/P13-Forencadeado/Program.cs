2338cfdad67ffa9583269ef700cc0777d3d74507
HarryCSantos <harry.santos@aluno.faculdadeimpacta.com.br>
2022-03-20 09:31:23 -0300

    alt

--- AprendendoCsharp/P13-Forencadeado/Program.cs
+++ AprendendoCsharp/P13-Forencadeado/Program.cs
@@ -8,6 +8,7 @@ namespace P13_Forencadeado
         {
             Console.WriteLine("Executando projeto 13");
 
+            // Escrevendo asteriscos com o Break
             for(int contadorLinha = 0; contadorLinha < 10; contadorLinha++)
             {
                 for(int contadorColuna = 0; contadorColuna <10; contadorColuna++)
@@ -17,6 +18,19 @@ namespace P13_Forencadeado
                         break;
                 }
                 Console.WriteLine(); 
+            }
+
+
+            // Uma forma diferente de fazer o desenho de asteriscos 
+            for (int contadorLinha = 0; contadorLinha < 10; contadorLinha++)
+            {
+                for (int contadorColuna = 0; contadorColuna < 10; contadorColuna++)
+                {
+                    Console.Write("*");
+                    if (contadorColuna >= contadorLinha)
+                        break;
+                }
+                Console.WriteLine();
             }
 
 

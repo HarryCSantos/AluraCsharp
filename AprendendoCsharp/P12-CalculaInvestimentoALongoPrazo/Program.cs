9fc07b86b667fa5e61b0743d8de48bafb0468bdf
HarryCSantos <harry.santos@aluno.faculdadeimpacta.com.br>
2022-03-20 08:38:30 -0300

    alt

--- AprendendoCsharp/P12-CalculaInvestimentoALongoPrazo/Program.cs
+++ AprendendoCsharp/P12-CalculaInvestimentoALongoPrazo/Program.cs
@@ -8,10 +8,15 @@ namespace P12_CalculaInvestimentoALongoPrazo
         {
             Console.WriteLine("Executando o projeto 12");
 
+            double valorInvestido = 1000;
             double fatorRendimento = 1.0036;
 
             for(int contadorAno = 1; contadorAno <= 5; contadorAno ++)
-            {
+            {   
+                for(int contadorMes = 1; contadorMes <= 12; contadorMes++)
+                {
+                    valorInvestido *= 0.0010;
+                }
                 fatorRendimento += 0.0010;
             }
 

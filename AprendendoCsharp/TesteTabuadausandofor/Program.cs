3fe7d5d17b7ae257c7ca4a3ec1b8f5ebee8df1ea
HarryCSantos <harry.santos@aluno.faculdadeimpacta.com.br>
2022-03-20 10:27:21 -0300

    alt

---
+++ AprendendoCsharp/TesteTabuadausandofor/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace TesteTabuadausandofor
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Executando - Tabuada");
+
+            for (int multiplicador = 1; multiplicador <= 10; multiplicador++)
+            {
+
+            }
+
+        }
+    }
+}

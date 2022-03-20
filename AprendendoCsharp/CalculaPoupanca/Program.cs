3bbe91ed168180f9fc884146a52ecca886542e0e
HarryCSantos <harry.santos@aluno.faculdadeimpacta.com.br>
2022-03-19 21:21:16 -0300

    alt

--- AprendendoCsharp/CalculaPoupanca/Program.cs
+++ AprendendoCsharp/CalculaPoupanca/Program.cs
@@ -16,7 +16,7 @@ namespace CalculaPoupanca
                 valorInvestido = valorInvestido + valorInvestido * 0.0036;
                 Console.WriteLine("Após " + contadorMes + " meses, você terá    R$ " + valorInvestido);
 
-                contadorMes = contadorMes + 1;
+                //contadorMes = contadorMes + 1;
             }
                 
 

d890b96a93db2600ed698080d35fe2931e544c62
HarryCSantos <harry.santos@aluno.faculdadeimpacta.com.br>
2022-03-22 22:39:00 -0300

    alt

--- AprendendoCsharp/LacoDeRepeticaoFor/Program.cs
+++ AprendendoCsharp/LacoDeRepeticaoFor/Program.cs
@@ -1,3 +1,28 @@
 ﻿using System;
-class Program
 
+class Programa
+{
+    static void Main(String [] args)
+    {
+        for (int i = 1; i < 100; i++)
+        {
+            if (i % 3 == 0)
+            {
+                Console.WriteLine(i);
+
+            }
+        }
+    }
+}*
+
+// Ou pode usar a primeira dentre as muitas opções
+class Programa
+{
+    static void Main(String [] args)
+    {
+        for (int i = 3; i < 100; i += 3)
+        {
+            Console.WriteLine(i);
+        }
+    }
+}
